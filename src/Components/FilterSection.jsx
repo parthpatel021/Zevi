27e99d2937d4b75f66ecff78771e69353adbebe8
Parth Patel <parthkumar111patel@gmail.com>
2023-12-27 12:02:54 +0530

    feat: add like and view-product button

--- src/Components/FilterSection.jsx
+++ src/Components/FilterSection.jsx
@@ -44,12 +44,12 @@ const FilterMenu = ({ filter }) => {
 
 const RatingFilter = ({ rating }) => {
     const [checked, setChecked] = React.useState(false);
-   
+
     const handleChange = () => {
         setChecked(!checked);
     };
 
-    
+
 
     return (
         <div className='filter'>
@@ -58,11 +58,31 @@ const RatingFilter = ({ rating }) => {
                 checked={checked}
                 inputProps={{ 'aria-label': 'controlled' }}
             />
-            
+
             <div className='star-w' onClick={handleChange}>
                 <Stars rating={rating} />
             </div>
 
+        </div>
+    )
+}
+
+const RatingFilterComponent = () => {
+    return (
+        <div className='filter-h-w'>
+            <div className='filter-top'>
+                <h4 className='filter-h'>Ratings</h4>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M12.71 14.8299L16.95 10.5899C17.0437 10.497 17.1181 10.3864 17.1689 10.2645C17.2197 10.1427 17.2458 10.012 17.2458 9.87994C17.2458 9.74793 17.2197 9.61723 17.1689 9.49537C17.1181 9.37351 17.0437 9.26291 16.95 9.16994C16.7626 8.98369 16.5092 8.87915 16.245 8.87915C15.9808 8.87915 15.7274 8.98369 15.54 9.16994L12 12.7099L8.45999 9.16994C8.27263 8.98369 8.01918 8.87915 7.75499 8.87915C7.4908 8.87915 7.23735 8.98369 7.04999 9.16994C6.95731 9.26338 6.88398 9.3742 6.83422 9.49604C6.78445 9.61787 6.75923 9.74834 6.75999 9.87994C6.75923 10.0115 6.78445 10.142 6.83422 10.2638C6.88398 10.3857 6.95731 10.4965 7.04999 10.5899L11.29 14.8299C11.383 14.9237 11.4936 14.9981 11.6154 15.0488C11.7373 15.0996 11.868 15.1257 12 15.1257C12.132 15.1257 12.2627 15.0996 12.3846 15.0488C12.5064 14.9981 12.617 14.9237 12.71 14.8299Z" fill="#2F3037" />
+                </svg>
+            </div>
+            <div className='star-w'>
+                {ratingFilters.map(rat => {
+                    return (
+                        <RatingFilter rating={rat} />
+                    )
+                })}
+            </div>
         </div>
     )
 }
@@ -73,23 +93,19 @@ const FilterSection = () => {
             {filterCatagoryData.map(({ name, filters }) => {
                 return (
                     <div className='filter-h-w'>
-                        <h4 className='filter-h'>{name}</h4>
+                        <div className='filter-top'>
+                            <h4 className='filter-h'>{name}</h4>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M12.71 14.8299L16.95 10.5899C17.0437 10.497 17.1181 10.3864 17.1689 10.2645C17.2197 10.1427 17.2458 10.012 17.2458 9.87994C17.2458 9.74793 17.2197 9.61723 17.1689 9.49537C17.1181 9.37351 17.0437 9.26291 16.95 9.16994C16.7626 8.98369 16.5092 8.87915 16.245 8.87915C15.9808 8.87915 15.7274 8.98369 15.54 9.16994L12 12.7099L8.45999 9.16994C8.27263 8.98369 8.01918 8.87915 7.75499 8.87915C7.4908 8.87915 7.23735 8.98369 7.04999 9.16994C6.95731 9.26338 6.88398 9.3742 6.83422 9.49604C6.78445 9.61787 6.75923 9.74834 6.75999 9.87994C6.75923 10.0115 6.78445 10.142 6.83422 10.2638C6.88398 10.3857 6.95731 10.4965 7.04999 10.5899L11.29 14.8299C11.383 14.9237 11.4936 14.9981 11.6154 15.0488C11.7373 15.0996 11.868 15.1257 12 15.1257C12.132 15.1257 12.2627 15.0996 12.3846 15.0488C12.5064 14.9981 12.617 14.9237 12.71 14.8299Z" fill="#2F3037" />
+                            </svg>
+                        </div>
                         {filters.map(filter => {
                             return <FilterMenu filter={filter} />
                         })}
                     </div>
                 )
             })}
-            <div className='filter-h-w'>
-                <h4 className='filter-h'>Ratings</h4>
-                <div className='star-w'>
-                    {ratingFilters.map(rat => {
-                        return (
-                            <RatingFilter rating={rat} />
-                        )
-                    })}
-                </div>
-            </div>
+            <RatingFilterComponent />
         </div>
     )
 }
--- src/Pages/ResultPage.jsx
+++ src/Pages/ResultPage.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from '../Components/SearchBar'
 import FilterSection from '../Components/FilterSection'
 import Stars from '../Components/Stars';
 import resultData from './../assets/resultData';
 
 import '../Styles/resultPage.css'
+import { Link } from 'react-router-dom';
 
 const ResultComponent = (props) => {
-    const {name, imgURL, prevPrice, currPrice, rating, reviewNumber} = props.result;
+    const { name, imgURL, prevPrice, currPrice, rating, reviewNumber } = props.result;
+    const [ liked, setLiked ] = useState(false);
+    const [ hover, setHover ] = useState(false);
 
     return (
-        <div className='result-w'>
+        <div className='result-w' onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
             <div className='like-w'>
-                <div className='like'></div>
+                <div className='like' onClick={() => setLiked(!liked)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                        <path d="M10.0938 5.01172C5.86581 5.01172 3.02734 8.43396 3.02734 12.6602C3.02734 17.4558 9.72721 23.1855 15.4688 27.0117C21.2103 23.1855 27.8327 17.3634 27.9102 12.6602C27.9798 8.43456 25.2436 5.01172 21.0156 5.01172C19.2119 5.01172 17.3826 5.96591 15.4688 7.87891C13.5549 5.96591 11.8975 5.01172 10.0938 5.01172Z" stroke={`${liked ? '' : 'white'}`} fill={`${liked ? '#D32424' : 'none'}`} />
+                    </svg>
+                </div>
                 <div className='product-img-w' style={{ backgroundImage: `url(${imgURL})` }}>
                 </div>
                 <div className="product-info">
@@ -20,6 +27,11 @@ const ResultComponent = (props) => {
                     <p className='prices'><span className='prev-price'>Rs.{prevPrice}</span> <span className='curr-price'>Rs.{currPrice}</span></p>
 
                     <p className='review'><Stars rating={rating} /> <span className='review-number'>({reviewNumber})</span></p>
+                </div>
+                <div className='view-product-btn-w' style={{display: `${hover ? 'flex' : 'none'}`}}>
+                    <Link className='view-product-btn'>
+                        View Product
+                    </Link>
                 </div>
             </div>
 
@@ -42,7 +54,7 @@ const ResultPage = () => {
                     </div>
                     <div className="results">
                         {resultData.map(result => {
-                            return(
+                            return (
                                 <ResultComponent result={result} />
                             )
                         })}
